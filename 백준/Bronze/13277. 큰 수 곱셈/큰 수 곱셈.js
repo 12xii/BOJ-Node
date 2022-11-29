ac39f0b937c9f529459e082a67e2c2ace1f36b17
Xii <117415639+12xii@users.noreply.github.com>
2022-11-29 09:43:14 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 904 ms, Memory: 14848 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.js
@@ -0,0 +1,8 @@
+const fs = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+
+const A = BigInt(fs[0]), B = BigInt(fs[1]);
+let arr = '';
+
+arr += A*B;
+arr.split('n');
+console.log(arr)
