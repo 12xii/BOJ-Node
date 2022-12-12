2c7a3225e57f02dd41365b0af992487041d92629
Xii <117415639+12xii@users.noreply.github.com>
2022-12-13 08:21:43 +0900

    [Bronze IV] Title: 이칙연산, Time: 120 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/15726. 이칙연산/이칙연산.js
@@ -0,0 +1,4 @@
+const fs = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+const a = Number(fs[0]), b = Number(fs[1]), c = Number(fs[2]);
+
+console.log(b > c ? Math.floor(a * b / c) : Math.floor(a / b * c));
